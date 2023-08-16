18698683d174d6f7770dd29367e7c99343899cb2
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-08-16 11:57:26 +0100

    added parallax scroll effect

--- Parallax/Views/Home.swift
+++ Parallax/Views/Home.swift
@@ -30,7 +30,7 @@ struct Home: View {
                     }
                     .padding(.horizontal, 15)
                     .padding(.vertical, 10)
-                    .background(Color.gray.opacity(0.1).cornerRadius(.infinity))
+                    .background(.ultraThinMaterial, in: .capsule)
                 }
                 
                 Text("Where do you want to \nTravel?")
@@ -43,24 +43,33 @@ struct Home: View {
                     let size = geometry.size
                     
                     ScrollView(.horizontal){
-                        HStack(spacing: 10) {
+                        HStack(spacing: 5) {
                             ForEach(TripCard.tripCards){ card in
                                 GeometryReader { proxy in
                                     let cardSize = proxy.size
+                                    let minX = min((proxy.frame(in: .scrollView).minX * 1.4), proxy.size.width * 1.4)
                                     
                                     Image(card.image)
                                         .resizable()
                                         .aspectRatio(contentMode: .fill)
+                                        .scaleEffect(1.25)
+                                        .offset(x: -minX)
                                         .frame(width: cardSize.width, height: cardSize.height, alignment: .center)
                                         .cornerRadius(15)
                                         .shadow(color: .black.opacity(0.25 ), radius: 8, x: 5, y:10)
                                 }
                                 .frame(width: size.width - 60, height: size.height - 50)
+                                .scrollTransition(.interactive, axis: .horizontal) { view, phase in
+                                    view
+                                        .scaleEffect(phase.isIdentity ? 1 : 0.95)
+                                }
                             }
                         }
                         .padding(.horizontal, 30)
+                        .scrollTargetLayout()
                         .frame(height: size.height, alignment: .top)
                     }
+                    .scrollTargetBehavior(.viewAligned)
                     .scrollIndicators(.hidden)
                 }
                 .frame(height: 500)
