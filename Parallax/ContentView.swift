a47fdf52e2aa228a181d06028db738575beff006
RMehdid <rmehdid@harbineldjazair.com>
2023-08-16 10:12:56 +0100

    Initial Commit

---
+++ Parallax/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Parallax
+//
+//  Created by Samy Mehdid on 16/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Parallax/ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by Samy Mehdid on 16/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
